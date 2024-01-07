954c9124ea8516562f8e79e0420af254910b3458
Berat Hazer <berathazer371@gmail.com>
2024-01-07 22:40:28 +0300

    son kodlar

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 # 20010011071 Berat Hazer
 # 20010011066 Burcu Gül
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay , classification_report, accuracy_score
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 
-def fillMissingValuesWithMean(data, featureLength):
 
-    for i in range(featureLength):
+def fill_missing_values_with_mean(data):
+
+    feature_length = len(data.columns) - 1
+    for i in range(feature_length):
         if is_binary_column_without_nan(data, i):
             data = fill_binary_column(data, i)
         else:
@@ -65,6 +68,7 @@ def calculate_min_max_normalization(data, index, new_min=0, new_max=1):
 
     return data
 
+
 def normalization(data):
 
     for i in range(len(data.columns)-1):
@@ -74,67 +78,30 @@ def normalization(data):
     return data
 
 
-def calculate_column_statistics(data):
-    column_statistics = {}
-
-    for column in data.columns:
-        column_data = data[column]
-        stats = {
-            "min": column_data.min(),
-            "max": column_data.max(),
-            "std": column_data.std()
-        }
-        column_statistics[column] = stats
-
-        # Sütun istatistiklerini yazdır
-        print(f"Sütun: {column}, Min: {stats['min']}, Max: {stats['max']}, Std: {stats['std']}")
-
-
-###############################################################
+#########################################################################################################
 
 # Verisetini yükleme ve eksik değerleri doldurma
 path = './water_potability.csv'
 dataset = pd.read_csv(path)
-featureLength = len(dataset.columns) - 1
-dataWithoutNormalization = fillMissingValuesWithMean(dataset.copy(), featureLength)
+
+dataWithoutNormalization = fill_missing_values_with_mean(dataset.copy())
 dataWithNormalization = normalization(dataWithoutNormalization.copy())
 
-# Veriyi özellikler ve etiketler olarak ayırma
-X_without_norm = dataWithoutNormalization.iloc[:, :-1].values
-y_without_norm = dataWithoutNormalization.iloc[:, -1].values
+
 X_with_norm = dataWithNormalization.iloc[:, :-1].values
 y_with_norm = dataWithNormalization.iloc[:, -1].values
 
-# Veriyi eğitim ve test setlerine ayırma
-X_train_without_norm, X_test_without_norm, y_train_without_norm, y_test_without_norm = train_test_split(
-    X_without_norm, y_without_norm, test_size=0.2, random_state=42)
 
 X_train_with_norm, X_test_with_norm, y_train_with_norm, y_test_with_norm = train_test_split(
     X_with_norm, y_with_norm, test_size=0.2, random_state=42)
 
-# Veriyi ölçeklendirme
-scaler = StandardScaler()
-X_train_without_norm = scaler.fit_transform(X_train_without_norm)
-X_test_without_norm = scaler.transform(X_test_without_norm)
-
+# veri ölçeklendirme işlemi sırasında özelliklerin ortalamasını 0,
+# standart sapmasını ise 1 yaparak standartlaştırma yapar.
 scaler = StandardScaler()
 X_train_with_norm = scaler.fit_transform(X_train_with_norm)
 X_test_with_norm = scaler.transform(X_test_with_norm)
 
-# Model oluşturma ve eğitme (Normalize Edilmemiş Veri)
-model_without_norm = Sequential([
-    Dense(128, activation='relu', input_shape=(X_train_without_norm.shape[1],)),
-    Dropout(0.5),
-    Dense(64, activation='relu'),
-    Dense(1, activation='sigmoid')
-])
 
-model_without_norm.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-model_without_norm.fit(X_train_without_norm, y_train_without_norm, epochs=10, batch_size=32, validation_data=(X_test_without_norm, y_test_without_norm))
-
-
-# Model oluşturma ve eğitme (Normalize Edilmiş Veri)
 model_with_norm = Sequential([
     Dense(128, activation='relu', input_shape=(X_train_with_norm.shape[1],)),
     Dropout(0.5),
@@ -142,54 +109,73 @@ model_with_norm = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
+# Farklı optimizerlar: adam, sgd, rmsprop, adagrad, adamax
+# İkili sınıflandırma olduğu için binary_crossentropy kullandık
+# Metrikler: accuracy, f1 score, recall ve precision olabilir
 model_with_norm.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model_with_norm.fit(X_train_with_norm, y_train_with_norm, epochs=10, batch_size=32, validation_data=(X_test_with_norm, y_test_with_norm))
 
-# Tahmin yapma
-y_pred_without_norm = (model_without_norm.predict(X_test_without_norm) > 0.5).astype(int)
+# epoch değeri küçük veri setleri için genellikle 10-50 arasında seçilir
+# batch_size için 32, 64, 128 gibi küçük değerlerle başlamak idealdir.
+model_with_norm.fit(
+    X_train_with_norm, y_train_with_norm, epochs=10, batch_size=32, validation_data=(X_test_with_norm, y_test_with_norm)
+)
+
+
+# Naive Bayes modelini oluşturma
+naive_bayes_model = GaussianNB()
+naive_bayes_model.fit(X_train_with_norm, y_train_with_norm)
+
+
+# Random Forest modelini oluşturma
+random_forest_model = RandomForestClassifier(n_estimators=100, random_state=42)
+random_forest_model.fit(X_train_with_norm, y_train_with_norm)
+
+
+# 3 Modeline kullanarak tahmin yapma
 y_pred_with_norm = (model_with_norm.predict(X_test_with_norm) > 0.5).astype(int)
+y_pred_naive_bayes = naive_bayes_model.predict(X_test_with_norm)
+y_pred_random_forest = random_forest_model.predict(X_test_with_norm)
 
-# Doğruluk hesaplama
-accuracy_without_norm = accuracy_score(y_test_without_norm, y_pred_without_norm)
+# Modellerin Doğruluklarının hesaplanması
 accuracy_with_norm = accuracy_score(y_test_with_norm, y_pred_with_norm)
+accuracy_naive_bayes = accuracy_score(y_test_with_norm, y_pred_naive_bayes)
+accuracy_random_forest = accuracy_score(y_test_with_norm, y_pred_random_forest)
 
-print("Normalize Edilmemiş Veri Doğruluğu:", accuracy_without_norm)
-print("Normalize Edilmiş Veri Doğruluğu:", accuracy_with_norm)
+print("\n\nÇok Katmanlı Ağ Doğruluk Oranı:", accuracy_with_norm)
+print("\nNaive Bayes Doğruluk Oranı:", accuracy_naive_bayes)
+print("\nRandom Forest Doğruluk Oranı:", accuracy_random_forest)
 
-
-
-# classification report hesaplama
-report_without_norm = classification_report(y_test_without_norm, y_pred_without_norm, target_names=['0', '1'])
-report_with_norm = classification_report(y_test_with_norm, y_pred_with_norm, target_names=['0', '1'])
-
-# Normalize Edilmemiş Veri için
-print("Normalize Edilmemiş Veri Classification Report:")
-print(report_without_norm)
-
-# Normalize Edilmiş Veri için
-print("Normalize Edilmiş Veri Classification Report:")
-print(report_with_norm)
+print("\n\n")
 
 
+# Bu raporlar zaten confusion matrixı kullanarak oluşturuluor
+print("Çok Katmanlı Ağ\n", classification_report(y_test_with_norm, y_pred_with_norm))
+print("\n\nNaive Bayes\n", classification_report(y_test_with_norm, y_pred_naive_bayes))
+print("\n\nRandom Forest\n", classification_report(y_test_with_norm, y_pred_random_forest))
 
 # Confusion Matrix hesaplama
-cm_without_norm = confusion_matrix(y_test_without_norm, y_pred_without_norm)
-cm_with_norm = confusion_matrix(y_test_with_norm, y_pred_with_norm)
+cm_with_norm_nn = confusion_matrix(y_test_with_norm, y_pred_with_norm)
+cm_with_norm_nb = confusion_matrix(y_test_with_norm, y_pred_naive_bayes)
+cm_with_norm_rf = confusion_matrix(y_test_with_norm, y_pred_random_forest)
 
 # Confusion Matrix'leri görselleştirme
-disp_without_norm = ConfusionMatrixDisplay(confusion_matrix=cm_without_norm, display_labels=[0, 1])
-disp_with_norm = ConfusionMatrixDisplay(confusion_matrix=cm_with_norm, display_labels=[0, 1])
+disp_with_norm_nn = ConfusionMatrixDisplay(confusion_matrix=cm_with_norm_nn, display_labels=[0, 1])
+disp_with_norm_nb = ConfusionMatrixDisplay(confusion_matrix=cm_with_norm_nb, display_labels=[0, 1])
+disp_with_norm_rf = ConfusionMatrixDisplay(confusion_matrix=cm_with_norm_rf, display_labels=[0, 1])
 
-# İki plot'u yan yana birleştirme
-fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+fig, axs = plt.subplots(1, 3, figsize=(18, 4))
 
-# Normalize Edilmemiş Veri Confusion Matrix
-disp_without_norm.plot(cmap='Blues', values_format='d', ax=axs[0])
-axs[0].set_title('Normalize Edilmemiş Veri Confusion Matrix')
+# Çok Katmanlı Yapay Sinir Ağı Confusion Matrix
+disp_with_norm_nn.plot(cmap='Blues', values_format='d', ax=axs[0])
+axs[0].set_title('NN Confusion Matrix')
 
-# Normalize Edilmiş Veri Confusion Matrix
-disp_with_norm.plot(cmap='Blues', values_format='d', ax=axs[1])
-axs[1].set_title('Normalize Edilmiş Veri Confusion Matrix')
+# Naive Bayes Confusion Matrix
+disp_with_norm_nb.plot(cmap='Blues', values_format='d', ax=axs[1])
+axs[1].set_title('Naive Bayes Confusion Matrix')
+
+# Random Forest Confusion Matrix
+disp_with_norm_rf.plot(cmap='Blues', values_format='d', ax=axs[2])
+axs[2].set_title('Random Forest Confusion Matrix')
 
 plt.show()
